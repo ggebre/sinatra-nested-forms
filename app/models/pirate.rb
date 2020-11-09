7cf594f724b3c39a25a8f65e8265a5a84ef86520
Getu Gebre <getu.gebre.gg@gmail.com>
2020-11-09 16:23:35 -0600

    done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+    @@pirates = []
+    attr_reader :name, :weight, :height
+    def initialize(pirate)
+        @name = pirate["name"]
+        @weight = pirate["weight"]
+        @height = pirate["height"]
+        @@pirates << self
+    end
+
+    def self.all
+        @@pirates
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    @@ships = []
+    attr_reader :name, :type, :booty
+
+    def initialize(ships)
+        @name = ships["name"]
+        @type = ships["type"]
+        @booty = ships["booty"]
+        @@ships << self
+    end
+
+    def self.all
+        @@ships
+    end
+
+    def self.clear
+        @@ships.clear
+    end
 end
